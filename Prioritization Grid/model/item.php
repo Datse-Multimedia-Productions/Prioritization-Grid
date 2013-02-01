0c9e4bdc4c833c34af7f2a6f1f1a1a96fc85a426
JigmeDatse <info@datsemultimedia.com>
2013-02-01 02:10:59 -0800

    Added item to model.
    model/item.php - Created the item class.  Needs much work.  Though the
    basics are there.
    
    Signed-off-by: JigmeDatse <info@datsemultimedia.com>

---
+++ Prioritization Grid/model/item.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Created on 2013-02-01
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+
+class item {
+	
+	public $name;
+	public $description;
+	public $score;
+	
+	public function compare (item $item, $prefered) {
+		if ($prefered) {
+			$score++
+		} else {
+			$item->score++;
+		}
+	}
+	
+}
+
+?>
